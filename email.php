892c1f2821a5a41a926f9240341a8b12f256a687
Evelym Rodriguez <evelym_r@hotmail.com>
2016-06-10 21:00:08 -0500

    tratando de resolver la comunicación en contacto.

--- email.php
+++ email.php
@@ -1,83 +1,128 @@
 <?php
-/**
-* Envio de email para contacto.
-*
-**/
-	$name = $_POST["name"];
-	$email = $_POST["email"];
-	$website = $_POST["website"] ? $_POST["website"] : "";
-	$message = $_POST["message"];
-
-	$destino = "evelym_r@hotmail.com";
-	$asunto = $name . "quiere contactar contigo desde tu portafolio";
-
-	$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
-	$cabeceras .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
-	$cabeceras .= 'From:' . $asunto . ' <' . $email . '> ' . "\r\n";
-
-	$html = "
-
-	<!DOCTYPE html>
-	<html lang=\"es\">
-	<head>
-		<meta charset=\"UTF-8\">
-		<title>Contacto de Portafolio</title>
-		<style>
-			body {
-				font-family: sans-serif,arial;
-				color: #555;
-			}
-			main {
-				width: 90%;
-				margin: auto;
-			}
-			.small {
-				font-size: .8rem;
-			}
-			.text-muted {
-				color: #aaa;
-			}
-			.text-right {
-				text-align: right;
-			}
-			a {
-				color: #3498db;
-				text-decoration: none;
-			}
-			a:hover {
-				text-decoration: underline;
-			}
-		</style>
-	</head>
-	<body>
-		<main>
-			<table class=\"table\">
-				<tbody>
-					<tr>
-						<td>
-							<p>Un mensaje de <strong>" . $name . "</strong>.</p>
-							<p>" . $message . "</p>
-							<br>
-							<p class=\"small text-right text-muted\">Email: " . $email . "</p>
-							<p class=\"small text-right text-muted\">URL: <a href=\"" . $website . "\" target=\"_blank\">" . $website . "</a></p>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</main>
-	</body>
-	</html>
-
-	";
-
-	if (isset($name) AND isset($email) AND isset($message)) {
-		if (!empty($name) AND !empty($email) AND !empty($message)) {
-			mail($destino,$asunto,$html,$cabeceras);
-			echo 1;
-		} else {
-			echo 0;
-		}
-	} else {
-		echo 0;
-	}
+if(isset($_POST['email'])) {
+ 
+    // 
+ 
+    $email_to = "evelym_r@hotmail.com";
+ 
+    $email_subject = "Contacto desde Web";
+ 
+    function died($error) {
+ 
+        // mensajes de error
+ 
+        echo "Lo sentimos, hubo un error en sus datos y el formulario no puede ser enviado en este momento. ";
+ 
+        echo "Detalle de los errores.<br /><br />";
+ 
+        echo $error."<br /><br />";
+ 
+        echo "Porfavor corrija estos errores e inténtelo de nuevo.<br /><br />";
+        die();
+    }
+ 
+    // Se valida que los campos del formulairo estén llenos
+ 
+    if(!isset($_POST['name']) ||
+ 
+        !isset($_POST['email']) ||
+ 
+        !isset($_POST['message'])) {
+ 
+        die('Lo sentimos pero parece haber un problema con los datos enviados.');
+ 
+    }
+ //En esta parte el valor "name"  sirve para crear las variables que recolectaran la información de cada campo
+ 
+    $first_name = $_POST['name']; // requerido
+ 
+    $email_from = $_POST['email']; // requerido 
+ 
+    $message = $_POST['message']; // requerido
+ 
+    $error_message = "";//Linea numero 52;
+ 
+//En esta parte se verifica que la dirección de correo sea válida 
+ 
+   $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+ 
+    $error_message .= 'La dirección de correo proporcionada no es válida.<br />';
+ 
+  }
+ 
+//En esta parte se validan las cadenas de texto
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$first_name)) {
+ 
+    $error_message .= 'El formato del nombre no es válido<br />';
+ 
+  }
+ 
+  if(!preg_match($string_exp,$last_name)) {
+ 
+    $error_message .= 'el formato del apellido no es válido.<br />';
+ 
+  }
+ 
+  if(strlen($message) < 2) {
+ 
+    $error_message .= 'El formato del texto no es válido.<br />';
+ 
+  }
+ 
+  if(strlen($error_message) > 0) {
+ 
+    die($error_message);
+ 
+  }
+ 
+//Este es el cuerpo del mensaje tal y como llegará al correo
+ 
+    $email_message = "Contenido del Mensaje.\n\n";
+ 
+ 
+ 
+    function clean_string($string) {
+ 
+      $bad = array("content-type","bcc:","to:","cc:","href");
+ 
+      return str_replace($bad,"",$string);
+ 
+    }
+ 
+ 
+ 
+    $email_message .= "Nombre: ".clean_string($name)."\n";
+ 
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+ 
+    $email_message .= "Mensaje: ".clean_string($message)."\n";
+ 
+ 
+//Se crean los encabezados del correo
+ 
+$headers = 'From: '.$email_from."\r\n".
+ 
+'Reply-To: '.$email_from."\r\n" .
+ 
+'X-Mailer: PHP/' . phpversion();
+ 
+@mail($email_to, $email_subject, $email_message, $headers);
+ 
+?>
+ 
+ 
+ 
+<!-- Mensaje de que fue enviado-->
+ 
+Gracias! Nos pondremos en contacto contigo a la brevedad
+ 
+<?php
+ 
+}
+ 
 ?>
